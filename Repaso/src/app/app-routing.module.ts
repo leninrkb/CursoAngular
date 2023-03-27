a64a62d522594d4f1675de76df020707cacbfc45
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-26 20:13:07 -0500

    routing producto

--- Repaso/src/app/app-routing.module.ts
+++ Repaso/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearEditarProductoComponent } from './components/crear-editar-producto/crear-editar-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:ListarProductosComponent},
+  {path:'crear', component:CrearEditarProductoComponent},
+  {path:'editar/:id', component:CrearEditarProductoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
