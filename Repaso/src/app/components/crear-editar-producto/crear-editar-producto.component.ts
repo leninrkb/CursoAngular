f4ed4ea0f940ed72d4176e74d05f8c3c056ff519
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-28 16:31:53 -0500

    captura de objeto y control de form

--- Repaso/src/app/components/crear-editar-producto/crear-editar-producto.component.ts
+++ Repaso/src/app/components/crear-editar-producto/crear-editar-producto.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Producto } from 'src/app/models/Producto';
 
 @Component({
   selector: 'app-crear-editar-producto',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CrearEditarProductoComponent {
   productoForm!:FormGroup;
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder, private router:Router){
     this.productoForm = fb.group({
       nombre: ['',Validators.required],
       categoria: ['',Validators.required],
@@ -19,6 +21,12 @@ export class CrearEditarProductoComponent {
   }
 
   agregarProducto(){
-    // console.log(this.productoForm.controls['nombre'].touched);
+    const PRODUCTO:Producto = {
+      nombre: this.productoForm.value.nombre,
+      categoria: this.productoForm.value.categoria,
+      ubicacion: this.productoForm.value.ubicacion,
+      precio: this.productoForm.value.precio,
+    }
+    this.router.navigate(['/']);
   }
 }
