99eeaf9c638c838e4fd4e03c9f3e89bfc81dd695
Lenin Acosta <rkb.lenin@gmail.com>
2023-03-31 15:46:09 -0500

    creo servicior y variables env

--- servidor/README.md
+++ servidor/README.md
@@ -4,4 +4,8 @@ npm init
 ## Instalar servidor de desarrollo
 npm install -D nodemon
 
+## Desarrollo
 npm install express mongoose dotenv
+
+## Mongo
+sudo service mongod start
--- servidor/index.js
+++ servidor/index.js
@@ -1,1 +1,13 @@
-console.log('dddd');
+const express = require('express');
+
+//creo servidor
+const app = express();
+
+//defino ruta principal
+app.get('/',(req, res) => {
+    res.send('funcionando')
+});
+
+app.listen(4000, () => {
+    console.log('servidor corriendo en 4000');
+});
